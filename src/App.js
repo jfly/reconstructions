893b2c19bfb10e18c41dbb9ec4010e94fdd02bb3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-09-17 21:22:17 -0700

    Dumping uncommitted changes
    Wow, these uncommitted changes have been sitting here since 2018, lol.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
+
+import YouTube from 'react-youtube';
+
+// TODO: Is it possible to retrieve this on a per video basis?
+const FRAMERATE = 25;
 
 class App extends Component {
   render() {
+    const opts = {
+      playerVars: {
+        controls: 0,
+        disablekb: 1,
+        iv_load_policy: 3,
+      }
+    };
+
     return (
       <div className={styles.app}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
+        <YouTube
+          videoId="NevGDFBfQGw"
+          opts={opts}
+          onReady={this._onReady}
+          onStateChange={this._onStateChange}
+        />
+        <div>
+          3d cube here?
+        </div>
+        <div className={styles.bottom}>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <button onClick={() => this._onClick(false)}>&lt;</button>
+          <button onClick={() => this._onClick(true)}>&gt;</button>
+        </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this._handleKeyDown);
+    window.addEventListener('blur', this._handleBlur);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this._handleKeyDown);
+    window.removeEventListener('blur', this._handleBlur);
+  }
+
+  // Keep the youtube iframe from stealing focus.
+  _handleBlur = (event) => {
+    if(document.activeElement instanceof HTMLIFrameElement) {
+      setTimeout(function() {
+        document.activeElement.blur();
+      }, 0);
+    }
+  }
+
+  _handleKeyDown = (event) => {
+    if(event.key === "ArrowLeft" || event.key === "Backspace") {
+      this._onClick(false);
+    } else {
+      this._onClick(true);
+    }
+  }
+
+  _onClick = (forward) => {
+    const {yt} = this.state;
+
+    let currentTime = yt.getCurrentTime();
+    let direction = forward ? 1 : -1;
+    let nextTime = currentTime + direction * (1.0 / FRAMERATE);
+    yt.seekTo(nextTime, true);
+  }
+
+  _onReady = (event) => {
+    this.setState({ yt: event.target });
+  }
+
+  _onStateChange = (event) => {
+    if(event.data === YouTube.PlayerState.PLAYING) {
+      event.target.pauseVideo();
+    }
   }
 }
 
